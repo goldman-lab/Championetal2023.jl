73f8f70bec0f598b93597fdc58c3b6fac9340ba3
Ben Lankow <blankow@users.noreply.github.com>
2023-05-01 11:10:57 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/Championetal2023.jl
@@ -0,0 +1,5 @@
+module Championetal2023
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Championetal2023
+using Test
+
+@testset "Championetal2023.jl" begin
+    # Write your tests here.
+end
